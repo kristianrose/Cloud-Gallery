f2c20a1cc029255e7aafb389eafc60bc5a128e5c
Kristian Rose <62854656+kristianrose@users.noreply.github.com>
2024-03-20 07:58:58 +0000

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -33,7 +33,7 @@ const Card = ({ item }) => {
   return (
     <div className="card bg-base-900 shadow-xl ">
       <figure className=" max-h-[30vh]">
-        <img src={item.imageUrl} alt="Shoes" />
+        <img src={item.imageUrl} alt="Shoes" onError="this.onerror=null;this.src='https://firebasestorage.googleapis.com/v0/b/frog-fb23f.appspot.com/o/vander%40gmail.com%2Ff7356d7d-0a74-4338-862a-681f29fadd4e.png?alt=media&token=148e0afc-1b9c-4648-9d86-0226cf72eb42';">
       </figure>
       <div className="">
         {/* <h2 className="card-title">Uploaded by:{item.userEmail}</h2> */}
